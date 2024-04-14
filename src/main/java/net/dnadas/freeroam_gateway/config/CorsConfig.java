f13d1bfeae5875fe561fc240cce39da3b1950b2e
DNadas98 <daniel.nadas.123@gmail.com>
2024-04-14 13:05:33 +0200

    add CORS config

---
+++ src/main/java/net/dnadas/freeroam_gateway/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package net.dnadas.freeroam_gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+  private final String FRONTEND_URL;
+
+  @Autowired
+  CorsConfig(@Value("${FRONTEND_URL}") String FRONTEND_URL) {
+    this.FRONTEND_URL = FRONTEND_URL;
+  }
+
+  @Bean
+  CorsWebFilter corsWebFilter() {
+    CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.setAllowedOrigins(Arrays.asList(FRONTEND_URL));
+    corsConfig.setMaxAge(8000L);
+    corsConfig.addAllowedMethod("GET");
+    corsConfig.addAllowedMethod("POST");
+    corsConfig.addAllowedMethod("PUT");
+    corsConfig.addAllowedMethod("PATCH");
+    corsConfig.addAllowedMethod("DELETE");
+    UrlBasedCorsConfigurationSource source =
+      new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfig);
+    return new CorsWebFilter(source);
+  }
+}
